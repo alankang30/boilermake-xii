b89b5e09a6129b889ac42f8e1dc520f862be309d
Alan Kang <alankang30@gmail.com>
2025-02-22 18:11:55 -0500

    QuestionList working with images

--- frontend/src/pages/classes/CS240.js
+++ frontend/src/pages/classes/CS240.js
@@ -1,10 +1,11 @@
 import ClassPage from '../../components/ui/ClassPage.js'
 import QuestionAnswerPage from '../../components/ui/QuestionPage.js'
+import QuestionList from '../../components/QuestionList.js'
 
 function CS240Page() {
   return (
     <ClassPage classnumber="CS240">
-      <QuestionAnswerPage question="What's 2+2" answer="4"/>
+      <QuestionList/>
     </ClassPage>
   )
 }
