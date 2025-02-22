c92b3837272095894b6332b167eb4a4d15bdca5b
njsumi <ngrutese@purdue.edu>
2025-02-22 15:19:39 -0500

    updated course page

--- frontend/src/components/layout/MainNavigation.js
+++ frontend/src/components/layout/MainNavigation.js
@@ -11,29 +11,6 @@ function MainNavigation() {
   return (
     <header className={classes.header}>
 
-      {/* <Link to='/'>
-        <div className="shape text squirrel-r-c63b1dd72dd6">
-          <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63b1dd72dd6" data-x="271" data-y="270">
-            <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-              <div className="paragraph-set root-0-paragraph-set-0">
-                <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                  <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0"
-                    style={{
-                      color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre", fontSize: "18px",
-                      textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-chivo-mono",
-                      "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Chivo Mono", fontStyle: "normal",
-                      fontWeight: 400
-                    }}
-                  >
-                    squirrel review
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link> */}
-
       <Link to='/'>
         {/*text: squirrel review*/}
         <div className={classes.squirrelreview}>
@@ -58,77 +35,6 @@ function MainNavigation() {
           <p>feedback</p>
         </div>
       </Link>
-
-      {/* <Link to="/classes">
-        <div className="shape text courses-c63e55b77d74">
-          <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63e55b77d74" data-x="1516" data-y="270">
-            <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-              <div className="paragraph-set root-0-paragraph-set-0">
-                <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                  <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0"
-                    style={{
-                      color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre", fontSize: "18px",
-                      textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-chivo-mono",
-                      "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Chivo Mono", fontStyle: "normal",
-                      fontWeight: 400
-                    }}
-                  >
-                    courses
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link> */}
-
-      {/*text: about*/}
-      {/* <Link to="/about">
-        <div className="shape text about-c63e26240e6e">
-          <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63e26240e6e" data-x="1286" data-y="270">
-            <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-              <div className="paragraph-set root-0-paragraph-set-0">
-                <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                  <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0"
-                    style={{
-                      color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre", fontSize: "18px",
-                      textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-chivo-mono",
-                      "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Chivo Mono", fontStyle: "normal",
-                      fontWeight: 400
-                    }}
-                  >
-                    about
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link> */}
-
-      {/* <Link to="/feedback">
-        <div className="shape text feedback-c63e30185bf2">
-          <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63e30185bf2" data-x="1388" data-y="270">
-            <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-              <div className="paragraph-set root-0-paragraph-set-0">
-                <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                  <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0"
-                    style={{
-                      color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre", fontSize: "18px",
-                      textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-chivo-mono",
-                      "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Chivo Mono", fontStyle: "normal",
-                      fontWeight: 400
-                    }}
-                  >
-                    feedback
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link> */}
-
 
     </header>
   );
--- frontend/src/pages/Classes.js
+++ frontend/src/pages/Classes.js
@@ -3,167 +3,25 @@ import './Classes.css';
 
 function ClassesPage() {
   return (
-    //<!-- frame: Board -->
-    <body>
-    {/* <div className="frame board"> */}
-      <MainNavigation />
-   
-      {/*text: COURSES*/}
-      <div className="shape text courses-c63b562e5892">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63b562e5892" data-x="323.99999999999994" data-y="405">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(17, 84, 74, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre",
-                    fontSize: "40px", textRendering: "geometricPrecision", caretColor: "rgba(17, 84, 74, 1)", textDecoration: "none", "--font-id": "gfont-chivo-mono",
-                    "--fills": '[["^ ", "~:fill-color", "#11544a", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Chivo Mono", fontStyle: "normal",
-                    fontWeight: 400 }}
-                  >
-                  COURSES
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="classpage">
+      <div class="header">
+        <MainNavigation />
       </div>
-      {/*rect: Rectangle*/}
-      <div className="shape rect rectangle-c63c0c2cba88">
+
+      
+      <div className="courses">
+        <p>COURSES</p>
       </div>
-      {/*rect: Rectangle*/}
-      <div className="shape rect rectangle-c63c3af5c9ad">
+
+      <div className="boxes">
+        <div className="rect1"></div>
+        <div className="rect2"></div>
+        <div className="rect3"></div>
       </div>
-      {/*rect: Rectangle*/}
-      <div className="shape rect rectangle-c63c3d89de0a">
-      </div>
-      {/*text: 250*/}
-      <div className="shape text c-250-c63d1034f910">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63d1034f910" data-x="1191" data-y="475">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(17, 84, 74, 0.2)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre",
-                    fontSize: "320px", textRendering: "geometricPrecision", caretColor: "rgba(17, 84, 74, 0.2)", textDecoration: "none",
-                    "--font-id": "gfont-contrail-one", "--fills": '[["^ ", "~:fill-color", "#11544a", "~:fill-opacity", 0.2]]', letterSpacing: "-30px",
-                    fontFamily: "Contrail One", fontStyle: "normal", fontWeight:400 }}
-                  >
-                  250
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*text: 182*/}
-      <div className="shape text c-182-c63c6a0ca3e2">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63c6a0ca3e2" data-x="334" data-y="475">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(17, 84, 74, 0.2)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre",
-                    fontSize: "320px", textRendering: "geometricPrecision", caretColor: "rgba(17, 84, 74, 0.2)", textDecoration: "none",
-                    "--font-id": "gfont-contrail-one", "--fills": '[["^ ", "~:fill-color", "#11544a", "~:fill-opacity", 0.2]]', letterSpacing: "-30px",
-                    fontFamily: "Contrail One", fontStyle: "normal", fontWeight:400 }}
-                  >
-                  182
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*text: 251*/}
-      <div className="shape text c-251-c63d07bf295e">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63d07bf295e" data-x="765" data-y="475">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(17, 84, 74, 0.2)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "pre", fontSize: "320px",
-                    textRendering: "geometricPrecision", caretColor: "rgba(17, 84, 74, 0.2)", textDecoration: "none", "--font-id": "gfont-contrail-one", 
-                    "--fills": '[["^ ", "~:fill-color", "#11544a", "~:fill-opacity", 0.2]]', letterSpacing: "-30px", fontFamily: "Contrail One", 
-                    fontStyle: "normal", fontWeight: 400}}
-                  >
-                  251
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*text: DISCRETE MATH*/}
-      <div className="shape text discrete-m-c63d4290e27c">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63d4290e27c" data-x="396" data-y="624">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "break-spaces",
-                    fontSize: "48px", textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-fjalla-one",
-                    "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity",1]]', letterSpacing: "0px", fontFamily: "Fjalla One", fontStyle: "normal", 
-                    fontWeight: 400
-                  }}
-                >
-                  DISCRETE MATH
-                </span>
-              </p>
-              <p className="paragraph root-0-paragraph-set-0-paragraph-1" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-1-text-0" 
-                  style={{ color: "rgba(17, 84, 74, 0.2)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "break-spaces", 
-                    fontSize: "48px", textRendering: "geometricPrecision", caretColor: "rgba(17, 84, 74, 0.2)", textDecoration: "none", "--font-id": "gfont-fjalla-one",
-                    "--fills": '[["^ ", "~:fill-color", "#11544a", "~:fill-opacity", 0.2]]', letterSpacing: "0px", fontFamily: "Fjalla One", fontStyle: "normal", 
-                    fontWeight: 400}}
-                  > 
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*text: DATA STRUCTURES AND ALGORITHMS*/}
-      <div className="shape text data-struc-c63da9dc5ff8">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63da9dc5ff8" data-x="799" data-y="599">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "break-spaces",
-                    fontSize: "48px", textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-fjalla-one",
-                    "--fills":'[["^ &quot", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Fjalla One",
-                    fontStyle: "normal", fontWeight: 400
-                  }}
-                >
-                  DATA STRUCTURES AND ALGORITHMS
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/*text: SYSTEMS PROGRAMMING*/}
-      <div className="shape text systems-pr-c63dc3585804">
-        <div className="text-node-html" id="html-text-node-fdba266e-932a-802f-8005-c63dc3585804" data-x="1195" data-y="599">
-          <div className="root rich-text root-0" style={{ display: "flex", whiteSpace: "break-spaces", alignItems: "flex-start" }} xmlns="http://www.w3.org/1999/xhtml">
-            <div className="paragraph-set root-0-paragraph-set-0">
-              <p className="paragraph root-0-paragraph-set-0-paragraph-0" dir="ltr">
-                <span className="text-node root-0-paragraph-set-0-paragraph-0-text-0" 
-                  style={{ color: "rgba(38, 58, 56, 1)", textTransform: "none", lineBreak: "auto", overflowWrap: "initial", whiteSpace: "break-spaces", 
-                    fontSize: "48px", textRendering: "geometricPrecision", caretColor: "rgba(38, 58, 56, 1)", textDecoration: "none", "--font-id": "gfont-fjalla-one",
-                    "--fills": '[["^ ", "~:fill-color", "#263a38", "~:fill-opacity", 1]]', letterSpacing: "0px", fontFamily: "Fjalla One",
-                    fontStyle: "normal", fontWeight: 400
-                  }}
-                >
-                  SYSTEMS PROGRAMMING
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/* </div> */}
-    </body>
+    </div>
+
+
+
   )
 }
 
