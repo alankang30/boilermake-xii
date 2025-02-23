682b77113c0fca1f692499cc919b5f22c2600963
njsumi <ngrutese@purdue.edu>
2025-02-23 02:45:44 -0500

    feedback page

--- frontend/src/pages/Feedback.js
+++ frontend/src/pages/Feedback.js
@@ -1,7 +1,33 @@
+import MainNavigation from '../components/layout/MainNavigation';
+import './Feedback.css';
+
+
 function FeedbackPage() {
   return (
-  <div>
-      feedback.
+    <div className="feedback">
+      <div class="header">
+        <MainNavigation />
+      </div>
+
+      <div className="page">
+        <div className="title">LEAVE A REVIEW</div>
+
+        <div className="textbox">
+          <div className="border">
+            <div className="dots">
+              <div className="dot1"></div>
+              <div className="dot2"></div>
+              <div className="dot3"></div>
+            </div>
+          </div>
+
+          <button className="submit">submit</button>
+
+        </div>
+
+        <div className="footer">we appreciate your feedback!</div>
+      </div>
+
     </div>
   )
 }
